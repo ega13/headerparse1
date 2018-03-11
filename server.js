63723cd04d882670132752ce853e7cf02cf00965
Glitch (grave-canvas) <none>
2018-03-11 19:16:26 +0000

    🎙💦 Checkpoint server.js:471758/41

--- server.js
+++ server.js
@@ -1,37 +1,32 @@
-// server.js
-// where your node app starts
 
-// init project
+var path = require('path');
+var accepts = require('accepts');
 var express = require('express');
+var useragent = require('express-useragent');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(useragent.express());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+// routing
+app.get("/", function (req, res) {
+  res.redirect('/api/whoami');
+  // response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
+app.get('/api/whoami', (req, res) => {
+  var lang = accepts(req);
+  var soft = "OS: " + req.useragent.os + ", Browser: " +req.useragent.browser;
+  var ipadr = req.ip;
+  res.json({
+    "ipaddress": ipadr,
+    "language": lang.languages()[0],
+    "software": soft
+  }).end();
 });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
